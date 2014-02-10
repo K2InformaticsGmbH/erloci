68f15d45b0a0f4484791311f72a17956994f10ba
Bikram Chatterjee <razorpeak@gmail.com>
2014-02-10 17:28:23 +0100

    more stdafx related cleanup

--- c_src/erloci_drv/cmd_thread_pool.cpp
+++ c_src/erloci_drv/cmd_thread_pool.cpp
@@ -12,8 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */ 
-#include "stdafx.h"
-
 #include "erl_interface.h"
 #include "ei.h"
 
--- c_src/erloci_drv/oci_marshal.cpp
+++ c_src/erloci_drv/oci_marshal.cpp
@@ -12,8 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */ 
-#include "stdafx.h"
-
 #include "oci_marshal.h"
 #include "erl_interface.h"
 
